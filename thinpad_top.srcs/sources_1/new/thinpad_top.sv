9c6191bcb7a1dd009be6fb474049fa5bf88cebfb
周韧平 <2021010699@secoder.net>
2023-12-14 16:08:47 +0800

    50M

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -226,7 +226,7 @@ module thinpad_top (
   logic sys_clk;
   logic sys_rst;
 
-  assign sys_clk = clk_75M;
+  assign sys_clk = clk_50M;
   assign sys_rst = reset_of_clk10M;
   logic        wbm0_cyc_o;
   logic        wbm0_stb_o;
@@ -918,7 +918,7 @@ module thinpad_top (
   // 串口控制器模�??
   // NOTE: 如果修改系统时钟频率，也要修改此处的时钟频率参数
   uart_controller #(
-      .CLK_FREQ(75_000_000),
+      .CLK_FREQ(50_000_000),
       .BAUD    (115200)
   ) uart_controller (
       .clk_i(sys_clk),
